f8adf431c661e55e579ea6803aa4b5b0ab7a32b3
Ryan Gordon <ryan.gordon@useallfive.com>
2022-05-20 11:34:24 -0700

    Meta data updates and hero configuration example

--- slices/Hero/index.js
+++ slices/Hero/index.js
@@ -1,19 +1,35 @@
 import React from 'react'
 import { PrismicRichText } from '@prismicio/react'
 import styles from './index.module.scss'
+import classNames from 'classnames'
 
 const Hero = ({ slice }) => {
+  switch (slice?.variation) {
+    // Add more variation cases here
+    default:
+      return <HeroDefault {...slice?.primary} />
+  }
+}
+
+/*
+ * ============================================
+ * Hero Default
+ * ============================================
+ * Description: A little something about the code below...
+ */
+
+const HeroDefault = ({ title, description }) => {
   return (
-    <section className={styles.hero}>
+    <section className={classNames(styles.hero, styles.heroDefault)}>
       <span className="title">
-        {slice.primary.title ? (
-          <PrismicRichText field={slice.primary.title} />
+        {title ? (
+          <PrismicRichText field={title} />
         ) : (
           <h2>Template slice, update me!</h2>
         )}
       </span>
-      {slice.primary.description ? (
-        <PrismicRichText field={slice.primary.description} />
+      {description ? (
+        <PrismicRichText field={description} />
       ) : (
         <p>start by editing this slice from inside Slice Machine!</p>
       )}
