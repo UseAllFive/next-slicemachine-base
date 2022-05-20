799c5423de636bb5649880216990dd04be7f5984
Ryan Gordon <ryan.gordon@useallfive.com>
2022-05-20 10:41:04 -0700

    Readme updates and document fix

--- README.md
+++ README.md
@@ -8,25 +8,29 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 npx @slicemachine/init
 ```
 
-2. Add all of the following content types in Prismic that are located in the `customtypes` directory. You can just copy and paste the JSON into the JSON editor in Prismic.
+2. Add all of the following content types in Prismic that are located in the `customtypes` directory. You can just copy and paste the JSON into the JSON editor in Prismic. If you fail to do this then you might get some errors that
+
+- `header`: Singleton with links and social icons
+- `footer`: Singleton with links
+- `page`: Repeatable content type for creating custom pages
 
 ## Getting Started
 
 First, run the development server:
 
 ```bash
-npm run dev
-# or
+yarn install
+```
+
+The following will concurrently run Slice Machine UI and Storybook together.
+
+```
 yarn dev
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
-
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
-
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+Development Site: [http://localhost:3000](http://localhost:3000)
+Slice Machine UI: [http://localhost:9999](http://localhost:9999)
+Storybook: [http://localhost:6006](http://localhost:6006)
 
 ## Learn More
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,21 +1,20 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 import { GA_TRACKING_ID } from 'lib/analytics'
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          {process.env.NODE_ENV === 'production' && (
-            <>
-              {/* Global Site Tag (gtag.js) - Google Analytics */}
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-              />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {process.env.NODE_ENV === 'production' && (
+          <>
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
@@ -25,16 +24,15 @@ export default class MyDocument extends Document {
                       referrer: document.referrer.split('?')[0],
                     });
                   `,
-                }}
-              />
-            </>
-          )}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+              }}
+            />
+          </>
+        )}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
